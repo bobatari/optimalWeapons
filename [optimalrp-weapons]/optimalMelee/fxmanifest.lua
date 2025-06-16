3414ccda45536adc16ab77d35dcc0f3fab6b4587
bobatari <aaronaordaz@gmail.com>
2025-06-16 10:52:14 -0700

    update
    updates

---
+++ [optimalrp-weapons]/optimalMelee/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'cerulean'
+games {'gta5'}
+author 'bobatari'
+
+files{
+	'metas/**/*.meta',
+	'weaponcomponents.meta',
+	'pedpersonality.meta',
+	'weaponanimations.meta',
+	'weaponarchetypes.meta',
+	'weapon_shiv.meta',
+	'weapon_katanas.meta',
+	'weapon_bats.meta'
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'metas/**/weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'metas/**/weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'metas/**/weaponanimations.meta'
+data_file 'PED_PERSONALITY_FILE' 'metas/**/pedpersonality.meta'
+data_file 'WEAPONINFO_FILE' 'metas/**/weapons.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+data_file 'WEAPONINFO_FILE' 'weapon_shiv.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_shiv.meta'
+data_file 'WEAPONINFO_FILE' 'weapon_katanas.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_katanas.meta'
+data_file 'WEAPONINFO_FILE' 'weapon_bats.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapon_bats.meta'
+
